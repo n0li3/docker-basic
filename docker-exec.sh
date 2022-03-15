3625b12f87746f35701676732126545335698d76
n0li3 <goatbooster@outlook.com>
2022-03-15 12:06:55 +0700

    docker exec

---
+++ docker-exec.sh
@@ -0,0 +1,6 @@
+#Exec command in a running container
+#docker container exec -it redist /bin/bash
+#-i keep input interactiv
+#-t agr for alocate terminal access
+#/bin/bash instance program code in container
+docker exec -it redis bash
