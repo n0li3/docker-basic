9fdd0fae6f72a33507cbdf8d15cddbb3670d592d
n0li3 <goatbooster@outlook.com>
2022-03-15 11:46:57 +0700

    docker image

---
+++ docker-images.sh
@@ -0,0 +1,12 @@
+#List docker images
+docker image ls
+docker images
+
+#Pull image or repository
+#docker pull redis:latest
+#docker image pull alpine:latest
+docker pull
+
+#Remove one or more image
+#docker image rm
+docker rmi
